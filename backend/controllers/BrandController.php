762321f4fe58889e66e5c66343ea7d3a1270d509
leijie2009737 <leijie_2009737@163.com>
2017-07-18 16:59:20 +0800

    brand的增删改查，做完了 但是删除执行的不是逻辑删除

--- backend/controllers/BrandController.php
+++ backend/controllers/BrandController.php
@@ -51,15 +51,52 @@ class BrandController extends \yii\web\Controller
     }
 
     //修改品牌
-    public function actionEdit()
+    public function actionEdit($id)
     {
-
+        $model =Brand::findOne($id);
+        $request =new Request();
+        if($request->isPost) {
+            //加载数据
+            $model->load($request->post());
+            //实例化文件上传对象
+            $model->logoFile = UploadedFile::getInstance($model, 'logoFile');
+            if ($model->validate()) {
+                if ($model->logoFile) {
+                    $dir = \Yii::getAlias('@webroot') . '/upload/brand' . date('Ymd');
+                    if (!is_dir($dir)) {
+                        mkdir($dir);
+                    }
+                    $fileName = '/upload/brand' . date('Ymd') . '/' . uniqid() . '.' . $model->logoFile->extension;
+                    //保存上传文件
+                    $model->logoFile->saveAs(\Yii::getAlias('@webroot') . $fileName, false);
+                    $model->logo = $fileName;
+                }
+                //保存
+                $model->save();
+                //跳转到列表页
+                return $this->redirect(['brand/index']);//当前控制器 index操作
+            } else {
+                //打印模型的验证错误信息
+                var_dump($model->getErrors());
+                exit;
+            }
+        }
+        return $this->render('add',['model'=>$model]);
     }
 
     //删除品牌
-    public function actionDel()
+    public function actionDel($id)
     {
-
+        $model =Brand::findOne($id);
+        //var_dump(\Yii::getAlias('@webroot'). $model->logo);exit;
+        //删除图片
+        if ($model->logo) {
+            unlink(\Yii::getAlias('@webroot') . $model->logo);
+        }
+        //删除该数据
+        $model->delete();
+        //跳转
+        return $this->redirect(['brand/index']);
     }
 
 }
--- backend/views/brand/index.php
+++ backend/views/brand/index.php
@@ -1,9 +1,24 @@
-<?php
-/* @var $this yii\web\View */
-?>
-<h1>brand/index</h1>
-
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<table class="table table-bordered table-condensed">
+    <?=\yii\helpers\Html::a('添加',['brand/add'],['class'=>'btn btn-sm btn-primary'])?>
+    <tr>
+        <th>ID</th>
+        <th>名称</th>
+        <th>简介</th>
+        <th>LOGO</th>
+        <th>排序</th>
+        <th>状态</th>
+        <th>操作</th>
+    </tr>
+    <?php foreach($models as $model): ?>
+        <tr>
+            <td><?=$model->id?></td>
+            <td><?=$model->name?></td>
+            <td><?=$model->intro?></td>
+            <td><?=\yii\bootstrap\Html::img($model->logo/*?$student->img:'/upload/default.png'*/,['height'=>50])?></td>
+            <td><?=$model->sort?></td>
+            <td><?=$model->statusoptions[$model->status]?></td>
+            <td><?=\yii\helpers\Html::a('修改',['brand/edit/','id'=>$model->id],['class'=>'btn btn-sm btn-warning'])?>
+                <?=\yii\helpers\Html::a('删除',['brand/del','id'=>$model->id],['class'=>'btn btn-sm btn-danger pull-right'])?></td>
+        </tr>
+    <?php endforeach;?>
+</table>
