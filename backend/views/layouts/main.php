3f1d62a3255c61ba7f78a1c61dedc6c7e052e304
leijie2009737 <leijie_2009737@163.com>
2017-07-22 11:03:20 +0800

    创建商品的4个表

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -29,14 +29,25 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => '商城后台管理',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
+        //['label' => 'Home', 'url' => ['/site/index']],
+        ['label'=>'品牌管理','items'=>[
+            ['label'=>'品牌列表','url'=>['brand/index']]
+        ]],
+        ['label'=>'文章管理','items'=>[
+            ['label'=>'文章列表','url'=>['article/index']],
+            ['label'=>'文章分类列表','url'=>['article-category/index']],
+        ]],
+        ['label'=>'商品管理','items'=>[
+            ['label'=>'商品分类列表','url'=>['goods-category/index']],
+            ['label'=>'商品列表','url'=>['goods/index']],
+        ]]
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
